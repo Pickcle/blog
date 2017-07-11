3ae0e79b547843e3dfdc075ffb6c7750e8f7d657
pickcle <348707253@qq.com>
2017-07-11 15:53:46 +0800

    add bg img

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <side-bar class="f-left" />
-    <router-view class="f-left" />
+    <img src="./assets/bg.jpg" />
+    <side-bar class="f-left p-rel c-default" />
+    <router-view class="f-left p-rel c-default" />
   </div>
 </template>
 
@@ -16,4 +17,11 @@
 </script>
 
 <style lang="sass" scoped>
+  img
+    position: absolute
+    top: 0
+    left: 0
+    height: 100%
+    width: 100%
+    object-fit: cover
 </style>
