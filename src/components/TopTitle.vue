2fb130f33ed5e61216fca53222ad048d56a8ff52
pickcle <348707253@qq.com>
2017-07-21 15:11:56 +0800

    update TopTitle

--- src/components/TopTitle.vue
+++ src/components/TopTitle.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Pickcle's Blog
+  <div class="t-center bg-black color-white">
+    <p class="title">Pickcle's Blog</p>
+    <p class="desc">Pickcle的个人网站，主要用于写博客，记录学习过程中遇到的问题</p>
   </div>
 </template>
 
@@ -10,4 +11,12 @@
 </script>
 
 <style lang="sass" scoped>
+  p
+    padding: 10px 0
+
+  .title
+    font-size: 26px
+
+  .desc
+    font-size: 16px
 </style>
