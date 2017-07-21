9b958c69951def6373cfb2538873c6115f087375
pickcle <348707253@qq.com>
2017-07-21 14:55:58 +0800

    import these components

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    SideBar
+  <div class="w-100 bg-white">
+    <top-title>
+    </top-title>
+    <navigation>
+    </navigation>
+    <personal-info>
+    </personal-info>
+    <more-link>
+    </more-link>
   </div>
 </template>
 
 <script>
+  import TopTitle from './TopTitle.vue'
+  import MoreLink from './MoreLink.vue'
+  import Navigation from './Navigation.vue'
+  import PersonalInfo from './PersonalInfo.vue'
+
   export default {
+    components: {
+      TopTitle,
+      MoreLink,
+      Navigation,
+      PersonalInfo
+    }
   }
 </script>
 
 <style lang="sass" scoped>
+
 </style>
