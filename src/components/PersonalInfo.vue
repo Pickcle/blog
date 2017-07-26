fed069140bc67358104d40bd84654c43933fe2f8
pickcle <348707253@qq.com>
2017-07-26 16:49:19 +0800

    update css

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Pickcle
+  <div class="personal-info t-center bg-white pad-20">
+    <img class="head" src="../images/head.png">
+    <p class="name">Pickcle</p>
+    <p class="desc">毕业于华东师范大学，前端码农，爱好网游、桌游，LOL电一钻三，农药王者</p>
   </div>
 </template>
 
@@ -10,4 +12,19 @@
 </script>
 
 <style lang="sass" scoped>
+  .personal-info
+    margin-top: 10px
+
+    .head
+      width: 120px
+      object-fit: cover
+
+    .name
+      padding: 5px
+      font-size: 14px
+
+    .desc
+      font-size: 12px
+      color: #999
+      line-height: 24px
 </style>
