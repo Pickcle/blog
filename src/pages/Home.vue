4b4c5bb881efe5f221b5c711901d3bd575a5bdf0
pickcle <348707253@qq.com>
2017-07-29 23:00:45 +0800

    test <article>

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,76 @@
 <template>
   <div>
-    Home
+    <article class="post post-type-normal " itemscope itemtype="http://schema.org/Article">
+      <link itemprop="mainEntityOfPage" href="https://pickcle.github.io/centos6/2017/07/27/vps+node+nginx%E9%85%8D%E7%BD%AE.html">
+      <span style="display:none" itemprop="author" itemscope itemtype="http://schema.org/Person">
+        <meta itemprop="name" content="羸弱的小金鱼">
+        <meta itemprop="description" content="">
+        <meta itemprop="image" content="/assets/head/sakura_min.jpg">
+      </span>
+
+      <span style="display:none" itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
+        <meta itemprop="name" content="羸弱的小金鱼">
+        <span style="display:none" itemprop="logo" itemscope itemtype="http://schema.org/ImageObject">
+          <img style="display:none;" itemprop="url image" alt="羸弱的小金鱼" src="">
+        </span>
+      </span>
+
+      <header class="post-header">
+        <h1 class="post-title" itemprop="name headline">
+          <a class="post-title-link" href="https://pickcle.github.io/centos6/2017/07/27/vps+node+nginx%E9%85%8D%E7%BD%AE.html" itemprop="url">
+            MongoDB被盗，黑客真能帮你恢复？
+          </a>
+        </h1>
+
+        <div class="post-meta">
+          <span class="post-time">
+            <span class="post-meta-item-icon">
+              <i class="fa fa-calendar-o"></i>
+            </span>
+
+            <span class="post-meta-item-text">发表于</span>
+
+            <time title="Post created" itemprop="dateCreated datePublished" datetime="2017-07-17T16:46:32+08:00">
+              2017-07-17
+            </time>
+          </span>
+
+          <span class="post-comments-count">
+            <span class="post-meta-divider">|</span>
+            <span class="post-meta-item-icon">
+              <i class="fa fa-comment-o"></i>
+            </span>
+            <a href="https://pickcle.github.io/centos6/2017/07/27/vps+node+nginx%E9%85%8D%E7%BD%AE.html" itemprop="discussionUrl">
+              <span class="post-comments-count ds-thread-count" data-thread-key="2017/07/17/MongoDB被盗，黑客真能帮你恢复？/" itemprop="commentCount"></span>
+            </a>
+          </span>
+
+         <span id="/2017/07/17/MongoDB被盗，黑客真能帮你恢复？/" class="leancloud_visitors" data-flag-title="MongoDB被盗，黑客真能帮你恢复？">
+           <span class="post-meta-divider">|</span>
+           <span class="post-meta-item-icon">
+             <i class="fa fa-eye"></i>
+           </span>
+           <span class="post-meta-item-text">阅读次数 </span>
+           <span class="leancloud-visitors-count"></span>
+         </span>
+        </div>
+      </header>
+      <div class="post-body" itemprop="articleBody">
+        <p>谈到MongoDB，想必大家都不陌生，这是一个基于分布式文件存储的数据库。由 C++ 语言编写。旨在为 WEB 应用提供可扩展的高性能数据存储解决方案。</p>
+        <p>它是介于关系数据库和非关系数据库之间的产品，是非关系数据库当中功能最丰富，最像关系数据库的，因此它得以快速发展。</p>
+          <!--noindex-->
+        <div class="post-more-link text-center">
+          <a class="btn" href="/2017/07/17/MongoDB被盗，黑客真能帮你恢复？/#more" rel="contents">
+            阅读全文 &raquo;
+          </a>
+        </div>
+          <!--/noindex-->
+      </div>
+
+      <footer class="post-footer">
+        <div class="post-eof"></div>
+      </footer>
+    </article>
   </div>
 </template>
 
