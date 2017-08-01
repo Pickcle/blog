1785e4718d366e7451a212319b9ae6c9b55ce6f9
pickcle <348707253@qq.com>
2017-08-01 18:30:11 +0800

    add read-blogs script

--- build/build.js
+++ build/build.js
@@ -13,6 +13,9 @@ var webpackConfig = require('./webpack.prod.conf')
 var spinner = ora('building for production...')
 spinner.start()
 
+var readBlogs = require('./read-blogs.js')
+readBlogs()
+
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
