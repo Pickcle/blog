c79becc2edf8ea7d049a171e9955ab4369dfde0f
pickcle <348707253@qq.com>
2017-08-03 16:32:12 +0800

    use template

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,12 @@
-<!-- <template>
+<template>
   <div>
     <div class="article-container bg-white">
       <article class="article" v-for="blog in blogs">
-        <a class="article-title p-rel" href="www.baidu.com">标题</a>
+        <a class="article-title p-rel" :href="blog.link">{{ blog.title }}</a>
         <div class="sub-title">
           <span>
             <i class="d-i-block fa fa-calendar-o"></i>
-            <span>发表于</span>
+            <span>发表于{{ blog.date }}</span>
           </span>
           <span>
             <span class="divider">|</span>
@@ -15,140 +15,23 @@
           <span>
             <span class="divider">|</span>
             <i class="d-i-block fa fa-eye"></i>
-            <span>阅读次数</span>
+            <span>阅读次数{{ blog.times }}</span>
           </span>
-        </div>
-        <div class="link">
-          <a class="more-read" href="www.baidu.com">阅读全文>></a>
         </div>
         <div class="line"></div>
       </article>
     </div>
   </div>
-</template> -->
+</template>
 
 <script>
   import utils from '../utils.js'
-  
+
   export default {
     data () {
       return {
         blogs: utils.getBlogsInfo()
       }
-    },
-
-    render (createElement) {
-      const blogNodes = this.blogs.map(blog => {
-        return createElement(
-          'article',
-          {
-            class: 'article'
-          },
-          [
-            createElement(
-              'a',
-              {
-                attrs: {
-                  href: blog.link
-                },
-                class: 'article-title p-rel c-pointer'
-              },
-              blog.title
-            ),
-            createElement(
-              'div',
-              {
-                class: 'sub-title'
-              },
-              [
-                createElement(
-                  'span',
-                  {},
-                  [
-                    createElement(
-                      'i',
-                      {
-                        class: 'd-i-block fa fa-calendar-o'
-                      }
-                    ),
-                    createElement(
-                      'span',
-                      {
-                        style: 'padding-left: 5px'
-                      },
-                      '发表于' + blog.date
-                    )
-                  ]
-                ),
-                createElement(
-                  'span',
-                  {},
-                  [
-                    createElement(
-                      'span',
-                      {
-                        class: 'divider'
-                      },
-                      '|'
-                    ),
-                    createElement(
-                      'i',
-                      {
-                        class: 'd-i-block fa fa-comment-o'
-                      }
-                    )
-                  ]
-                ),
-                createElement(
-                  'span',
-                  {},
-                  [
-                    createElement(
-                      'span',
-                      {
-                        class: 'divider'
-                      },
-                      '|'
-                    ),
-                    createElement(
-                      'i',
-                      {
-                        class: 'd-i-block fa fa-eye'
-                      }
-                    ),
-                    createElement(
-                      'span',
-                      {
-                        style: 'padding-left: 5px'
-                      },
-                      '阅读次数' + blog.times
-                    )
-                  ]
-                )
-              ]
-            ),
-            createElement(
-              'div',
-              {
-                class: 'line'
-              }
-            )
-          ]
-        )
-      })
-      return createElement(
-        'div',
-        {},
-        [
-          createElement(
-            'div',
-            {
-              class: 'article-container bg-white'
-            },
-            blogNodes
-          )
-        ]
-      )
     }
   }
 </script>
@@ -189,18 +72,5 @@
 
       .line
         border-bottom: 1px solid #555
-
-      // .link
-      //
-      //   .more-read
-      //     font-size: 14px
-      //     border: 2px solid #222
-      //     padding: 2px 5px
-      //     transition: background-color 0.2s
-      //     transition: color 0.2s
-      //
-      //     &:hover
-      //       background-color: #222
-      //       color: #fff
 
 </style>
