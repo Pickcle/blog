f865fe079dd6498f99542b043cf8a665ccaf950a
pickcle <348707253@qq.com>
2017-08-09 17:30:26 +0800

    PageFooter

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="page-footer">
-    Footer
+  <div class="page-footer t-center w-100">
+    ©2013-{{ nowYear }} | Pickcle
   </div>
 </template>
 
 <script>
   export default {
+    computed: {
+      nowYear () {
+        return (new Date()).getFullYear()
+      }
+    }
   }
 </script>
 
 <style lang="sass" scoped>
   .page-footer
-    margin: 0 auto
-    width: 100px
     height: 50px
+    line-height: 50px
+    color: #333
+    background-color: #f5f7f9
 </style>
