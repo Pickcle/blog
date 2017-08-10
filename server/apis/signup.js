4a6efd6db108929501ac19c3b37715d7a4643261
pickcle <348707253@qq.com>
2017-08-10 11:28:15 +0800

    signup api

---
+++ server/apis/signup.js
@@ -0,0 +1,36 @@
+var connectDb = require('../mongodb/connect.js')
+
+/**
+ * @return 0：数据库操作失败，1：已注册过，2：注册成功
+ */
+const signup = (req, res) => {
+  const user_name = req.query.user_name
+  const password = req.query.password
+
+  connectDb((err, db) => {
+    db.collection('user').findOne({ user_name }, (err, doc) => {
+      if (doc) {
+        res.write(JSON.stringify({ result: 1 }))
+        res.end()
+        db.close()
+      } else {
+        connectDb((err, db) => {
+          db.collection('user').insert({
+            user_name,
+            password
+          }, (err, result) => {
+            if (err) {
+              res.write(JSON.stringify({ result: 0 }))
+            } else {
+              res.write(JSON.stringify({ result: 2 }))
+            }
+            res.end()
+            db.close()
+          })
+        })
+      }
+    })
+  })
+}
+
+module.exports = exports = signup
