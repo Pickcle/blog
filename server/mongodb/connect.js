c9a0321268e46d86d82f86a57f5f865efd5b36ab
pickcle <348707253@qq.com>
2017-08-10 11:29:49 +0800

    export connect api

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,18 +1,23 @@
 var MongoClient = require('mongodb').MongoClient
-var DB_CONN_STR = 'mongodb://localhost:27017/node'
+var NODE_DB = 'mongodb://localhost:27017/node'
 
-var mc = MongoClient.connect(DB_CONN_STR)
+function connectDb (callback) {
+  MongoClient.connect(NODE_DB, callback)
+}
 
-mc.then((db) => {
-  // console.log(db)
-  db.collection('user').insert({ a:1, b:2 })
-})
+module.exports = exports = connectDb
 
-setInterval(() => {
-  mc.then((db) => {
-    // console.log(db)
-    db.collection('user').find().toArray(function (err, result) {
-      console.log('result2:', result)
-    })
-  })
-}, 3000)
+// var mc = MongoClient.connect(DB_CONN_STR)
+// mc.then((db) => {
+//   // console.log(db)
+//   db.collection('user').insert({ a:1, b:2 })
+// })
+//
+// setInterval(() => {
+//   mc.then((db) => {
+//     // console.log(db)
+//     db.collection('user').find().toArray(function (err, result) {
+//       console.log('result2:', result)
+//     })
+//   })
+// }, 3000)
