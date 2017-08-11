29d0d2f743b4d2fd106b3674a9af7ad9252c274f
pickcle <348707253@qq.com>
2017-08-11 10:56:33 +0800

    add a link of source code

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page-footer t-center w-100">
-    ©2013-{{ nowYear }} | Pickcle
+    <p>©2013-{{ nowYear }} | Pickcle</p>
+    <a class="link" href="https://github.com/Pickcle/blog" target="_blank">source code</a>
   </div>
 </template>
 
@@ -16,8 +17,13 @@
 
 <style lang="sass" scoped>
   .page-footer
-    height: 50px
-    line-height: 50px
+    font-size: 16px
+    margin-top: 20px
+    height: 100px
+    line-height: 30px
     color: #333
-    background-color: #f5f7f9
+    // background-color: #f5f7f9
+
+    .link
+      color: #23ade6
 </style>
