986030ea0343c04eb7af182119bb9c9907fe57f6
Pickcle <348707253@qq.com>
2017-08-16 10:19:44 +0800

    delete test code

--- server/apis/getBlogInfo.js
+++ server/apis/getBlogInfo.js
@@ -2,22 +2,22 @@ var connectDb = require('../mongodb/connect.js')
 var DB = require('../mongodb/DB.js')
 
 const getBlogInfo = (req, res) => {
-  connectDb(DB.NODE, (err, db) => {
-    db.collection('blog').find({}).toArray(function (err, result) {
-      if (err) {
-        res.write(JSON.stringify({ ok: 0 }))
-        res.end()
-        return
-      }
+	connectDb(DB.NODE, (err, db) => {
+	  db.collection('blog').find({}).toArray(function (err, result) {
+	    if (err) {
+	      res.write(JSON.stringify({ ok: 0 }))
+	      res.end()
+	      return
+	    }
 
-      result.forEach(blogInfo => {
-        delete blogInfo['_id']
-      })
+	    result.forEach(blogInfo => {
+	      delete blogInfo['_id']
+	    })
 
-      res.write(JSON.stringify({ ok: 1, 'abc': '测试', 'result': result }))
-      res.end()
-    })
-  })
+	    res.write(JSON.stringify({ ok: 1, 'result': result }))
+	    res.end()
+	  })
+	})
 }
 
 module.exports = exports = getBlogInfo
